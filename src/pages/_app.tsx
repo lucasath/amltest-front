7f77fbc2cda2d33ebbffb27ac68973ce8737c0ff
Lucas Anthony Carvalho Brito <lucasath@hotmail.com>
2025-09-19 15:12:29 -0300

    fix: ajustando pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import { FormProvider } from "@/context/formContext";
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <FormProvider>
+        <Component {...pageProps} />
+      </FormProvider>
+    </QueryClientProvider>);
 }
